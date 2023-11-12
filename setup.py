fa7dc695daf1cbae01e8571aa5d9f32cbf4839b4
Bowie <marketing@letscode.com.br>
2023-11-11 23:43:07 -0300

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The following code is a script developed for a challenging competition in Solafune's Data Science area, focused on detecting solar panels in satellite images.',
+    author='Diovani Dupont',
+    license='',
+)
